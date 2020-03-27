40fd6f514f370bc725b759afdc3ce36b8f7a0102
Hemanth Devarapalli <itsmehemant123@users.noreply.github.com>
2020-03-27 15:34:46 -0400

    Merge zoom-test branch into master (#1)
    * Updated file for new branch
    
    * Create Sean_text.txt
    
    * Update test.py
    
    * Update test.py
    
    * Added new file for new commit
    
    * Commit second text edit
    
    * Added my line
    
    * updated test2 again
    
    * Update test.py
    
    * Create test_vlr.py
    
    * Commit sean change to test.py
    
    * Update test.py
    
    * fixed merge
    
    Co-authored-by: srothenb <srothenb.purdue@gmail.com>
    Co-authored-by: kimst12 <50676369+kimst12@users.noreply.github.com>
    Co-authored-by: vrayz <60087840+vrayz@users.noreply.github.com>

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
 print('Hello world')
+
+print('Hello world, zoom test branch')
+
+print('This is Kims addition')
+
+print('This is Vitaliy's line')
+print('Hello world from Sean')
---
+++ test_vlr.py
@@ -0,0 +1,7 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 26 2018, 23:26:24) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+print('hello world 3')
+hello world 3
+>>> 
